04e3a253944341c9aba112fd490220f575db3bb7
Dorian Zedler <dorian@itsblue.de>
2023-05-27 13:06:10 +0200

    Fix: pinout

--- display/src/main.cpp
+++ display/src/main.cpp
@@ -29,13 +29,13 @@
 #define A_PIN 23
 #define B_PIN 19
 #define C_PIN 5
-#define D_PIN 17
+#define D_PIN 18
 #define E_PIN                                                                  \
   32 // required for 1/32 scan panels, like 64x64px. Any available pin would do,
      // i.e. IO32
 #define LAT_PIN 4
 #define OE_PIN 15
-#define CLK_PIN 16
+#define CLK_PIN 22
 
 HUB75_I2S_CFG::i2s_pins _pins = {R1_PIN, G1_PIN,  B1_PIN, R2_PIN, G2_PIN,
                                  B2_PIN, A_PIN,   B_PIN,  C_PIN,  D_PIN,
